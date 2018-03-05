557eb5bb184e447e7767a43a873b8b375bc96fff
Alexandre Skipper <alexandreskipper@windows2.scs.carleton.ca>
2018-03-05 15:46:38 -0500

    it moves

---
+++ Arboles Peligrosas/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+    }
+}
+
---
+++ Arboles Peligrosas/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector3 destination;
+    private float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        destination = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0)) //left mouse button
+            destination = setDestination(Camera.main.ScreenPointToRay(Input.mousePosition));
+
+        movePlayer();
+	}
+
+    void movePlayer()
+    {
+        if (this.transform.position == destination) return;
+
+        this.transform.SetPositionAndRotation(destination, new Quaternion());
+    }
+
+    Vector3 setDestination(Ray ray)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            return (hit.point + new Vector3(0, 0.5f, 0));
+        }
+
+        return new Vector3(); //shouldn't happen
+    }
+}
