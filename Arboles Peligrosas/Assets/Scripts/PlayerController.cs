e93f8206e9da44e3b3aaa2cbb28e74cec11be9be
Alexandre Skipper <alexandreskipper@windows2.scs.carleton.ca>
2018-03-05 16:19:04 -0500

    it moves better now

--- Arboles Peligrosas/Assets/Scripts/PlayerController.cs
+++ Arboles Peligrosas/Assets/Scripts/PlayerController.cs
@@ -4,40 +4,67 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     private Vector3 destination;
     private float speed;
+    private Lerp movement;
 
 	// Use this for initialization
 	void Start ()
     {
-        destination = this.transform.position;
+        speed = 5.0f;
+        destination = transform.position;
+        movement = new Lerp(transform.position, transform.position, speed);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         if (Input.GetMouseButtonDown(0)) //left mouse button
-            destination = setDestination(Camera.main.ScreenPointToRay(Input.mousePosition));
+            setDestination(Camera.main.ScreenPointToRay(Input.mousePosition));
 
         movePlayer();
 	}
 
     void movePlayer()
     {
-        if (this.transform.position == destination) return;
-
-        this.transform.SetPositionAndRotation(destination, new Quaternion());
+        if (transform.position == destination) return;
+        
+        transform.position = movement.getPosition();
     }
 
-    Vector3 setDestination(Ray ray)
+    void setDestination(Ray ray)
     {
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
-            return (hit.point + new Vector3(0, 0.5f, 0));
+            destination = hit.point + new Vector3(0, 0.5f, 0);
         }
 
-        return new Vector3(); //shouldn't happen
+        movement = new Lerp(transform.position, destination, speed);
+    }
+}
+
+class Lerp
+{
+    private Vector3 start, end;
+    private float distance, timeToEnd, currTime;
+
+    public Lerp(Vector3 s, Vector3 e, float speed)
+    {
+        start = s;
+        end = e;
+        distance = Vector3.Distance(s, e);
+        timeToEnd = distance / speed;
+        currTime = 0;
+    }
+
+    public Vector3 getPosition()
+    {
+        currTime += Time.deltaTime;
+
+        if (currTime >= timeToEnd)
+            currTime = timeToEnd;
+
+        return Vector3.Lerp(start, end, currTime/timeToEnd);
     }
 }
