c2b6a6f2bbe2a9fd9db24893f64ed1e4af16d7de
Alexandre Skipper <alexandreskipper@WINDOWS2.SCS.CARLETON.CA>
2018-03-05 16:38:02 -0500

    HERE

--- Arboles Peligrosas/Assets/Scripts/PlayerController.cs
+++ Arboles Peligrosas/Assets/Scripts/PlayerController.cs
@@ -44,27 +44,3 @@ public class PlayerController : MonoBehaviour
     }
 }
 
-class Lerp
-{
-    private Vector3 start, end;
-    private float distance, timeToEnd, currTime;
-
-    public Lerp(Vector3 s, Vector3 e, float speed)
-    {
-        start = s;
-        end = e;
-        distance = Vector3.Distance(s, e);
-        timeToEnd = distance / speed;
-        currTime = 0;
-    }
-
-    public Vector3 getPosition()
-    {
-        currTime += Time.deltaTime;
-
-        if (currTime >= timeToEnd)
-            currTime = timeToEnd;
-
-        return Vector3.Lerp(start, end, currTime/timeToEnd);
-    }
-}
