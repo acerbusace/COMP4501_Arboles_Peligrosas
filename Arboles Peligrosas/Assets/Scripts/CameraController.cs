d3698471258228afc217f1a171bc5ee2747c87ab
Alexkumar Patel <acerbus.ace@gmail.com>
2018-03-08 02:49:48 -0500

    added camera acceleration

--- Arboles Peligrosas/Assets/Scripts/CameraController.cs
+++ Arboles Peligrosas/Assets/Scripts/CameraController.cs
@@ -4,42 +4,55 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private float speed = 10f;
-    private float scrollSpeed = 10f;
+    public float acceleration;
+    public float deceleration;
+
+    private Vector3 velocity = new Vector3();
+    private float maxVelocity = 100f;
     private float minDist = 10f;
     private float maxDist = 35f;
-    private float sensitivity = 10f;
 
-    private float zSnapOffset = -15;
+    private float scrollSensitivity = 100f;
+
+    private float zSnapOffset = -15; // z-axis offset for camera snapping
 
     public SelectController selectController;
 
     void Update()
     {
+        // Spacebar is Pressed
+        // Then snap to the last selected unit
         if (Input.GetKeyDown(KeyCode.Space)) {
             GameObject unit = selectController.getSelctedFrame();
-            if (unit != null)
+            if (unit != null) {
                 transform.position = new Vector3(unit.transform.position.x, transform.position.y, unit.transform.position.z + zSnapOffset);
+                velocity = new Vector3();
+            }
         }
 
+        // sets velocity based on input axis
+        velocity += new Vector3(
+            Input.GetAxis("Horizontal") * acceleration * Time.deltaTime,
+            -(Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity * acceleration * Time.deltaTime), 
+            Input.GetAxis("Vertical") * acceleration * Time.deltaTime
+        );
 
-        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        // make sure velocity doesn't exceed max value
+        if (velocity.x > maxVelocity) velocity.x = maxVelocity;
+        if (velocity.z > maxVelocity) velocity.z = maxVelocity;
 
-        transform.position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+        // decelerate, if input axis are not pressed
+        if (Input.GetAxis("Horizontal") == 0)
+            velocity.x /= 1f + (deceleration * Time.deltaTime);
+        if (Input.GetAxis("Vertical") == 0)
+            velocity.z /= 1f + (deceleration * Time.deltaTime);
+        if (Input.GetAxis("Mouse ScrollWheel") == 0)
+            velocity.y /= 1f + (deceleration * Time.deltaTime);
 
-        //float fov = Camera.main.fieldOfView;
-        //fov += -Input.GetAxis("Mouse ScrollWheel") * sensitivity;
-        //fov = Mathf.Clamp(fov, minDist, maxDist);
-        //Camera.main.fieldOfView = fov;
+        transform.position += velocity * Time.deltaTime;
 
-        float scroll = Input.GetAxis("Mouse ScrollWheel") * sensitivity;
-        if (scroll != 0f) {
-            float y = scrollSpeed * speed * Time.deltaTime;
-            if (scroll > 0f && transform.position.y > minDist)
-                transform.position = new Vector3(transform.position.x, transform.position.y - y, transform.position.z);
-            else if (scroll < 0f && transform.position.y < maxDist)
-                transform.position = new Vector3(transform.position.x, transform.position.y + y, transform.position.z);
-        }
+        // make sure camera's y position is not out of min/max distance
+        if (transform.position.y > maxDist) transform.position = new Vector3(transform.position.x, maxDist, transform.position.z);
+        if (transform.position.y < minDist) transform.position = new Vector3(transform.position.x, minDist, transform.position.z);
     }
 }
