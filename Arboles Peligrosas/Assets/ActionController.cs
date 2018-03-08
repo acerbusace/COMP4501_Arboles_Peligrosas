ad42aad1c9267986274b33d0d637d52b9d47e751
Alexkumar Patel <acerbus.ace@gmail.com>
2018-03-08 12:31:08 -0500

    refactored select controller

--- Arboles Peligrosas/Assets/ActionController.cs
+++ Arboles Peligrosas/Assets/ActionController.cs
@@ -4,13 +4,73 @@ using UnityEngine;
 
 public class ActionController : MonoBehaviour {
 
+    private List<string> resourseTags;
+    private bool shiftModifier;
+    public SelectController selectController;
+
 	// Use this for initialization
 	void Start () {
-		
+        resourseTags = new List<string>();
+        resourseTags.Add("Tree");
+        resourseTags.Add("Stone");
+
+        shiftModifier = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            shiftModifier = true;
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            shiftModifier = false;
+
+        List<GameObject> selectedFrames = selectController.getSelectedFrames();
+
+        if (Input.GetMouseButtonDown(1)) { //right mouse button
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (HelperFunctions.containsTag(resourseTags, hit.transform.gameObject.tag))
+                {
+                    if (shiftModifier)
+                    {
+                        foreach (GameObject sf in selectedFrames)
+                        {
+                            if (HelperFunctions.containsTag("Gatherable", sf.gameObject.tag))
+                                sf.GetComponent<Moveable>().queueGather(hit);
+                        }
+                    }
+                    else
+                    {
+                        foreach (GameObject sf in selectedFrames)
+                        {
+                            if (HelperFunctions.containsTag("Gatherable", sf.gameObject.tag))
+                                sf.GetComponent<Moveable>().gather(hit);
+                        }
+                    }
+                }
+                else
+                {
+                    if (shiftModifier)
+                    {
+                        foreach (GameObject sf in selectedFrames)
+                        {
+                            if (HelperFunctions.containsTag("Moveable", sf.gameObject.tag))
+                                sf.GetComponent<Moveable>().queueMove(hit);
+                        }
+                    }
+                    else
+                    {
+                        foreach (GameObject sf in selectedFrames)
+                        {
+                            if (HelperFunctions.containsTag("Moveable", sf.gameObject.tag))
+                                sf.GetComponent<Moveable>().move(hit);
+                        }
+                    }
+                }
+            }
+        }
 	}
 }
--- Arboles Peligrosas/Assets/Scripts/SelectController.cs
+++ Arboles Peligrosas/Assets/Scripts/SelectController.cs
@@ -6,9 +6,7 @@ using UnityEngine.UI;
 public class SelectController : MonoBehaviour {
 
     private List<GameObject> selectedFrames;
-    private UI_SelectedFrame sfInfo;
     private string sfTag;
-    private List<string> resourseTags;
     // used for multi select and queueing actions
     private bool shiftModifier;
 
@@ -17,10 +15,6 @@ public class SelectController : MonoBehaviour {
         selectedFrames = new List<GameObject>();
 
         sfTag = "Selectable";
-
-        resourseTags = new List<string>();
-        resourseTags.Add("Tree");
-        resourseTags.Add("Stone");
 
         shiftModifier = false;
     }
@@ -32,54 +26,9 @@ public class SelectController : MonoBehaviour {
         if (Input.GetKeyUp(KeyCode.LeftShift))
             shiftModifier = false;
 
+        //left mouse button
         if (Input.GetMouseButtonDown(0)) {
-            //left mouse button
             setSelectableUnit(Camera.main.ScreenPointToRay(Input.mousePosition));
-        } else if (Input.GetMouseButtonDown(1)) { //right mouse button
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (HelperFunctions.containsTag(resourseTags, hit.transform.gameObject.tag))
-                {
-                    if (shiftModifier)
-                    {
-                        foreach (GameObject sf in selectedFrames)
-                        {
-                            if (HelperFunctions.containsTag("Gatherable", sf.gameObject.tag))
-                                sf.GetComponent<Moveable>().queueGather(hit);
-                        }
-                    }
-                    else
-                    {
-                        foreach (GameObject sf in selectedFrames)
-                        {
-                            if (HelperFunctions.containsTag("Gatherable", sf.gameObject.tag))
-                                sf.GetComponent<Moveable>().gather(hit);
-                        }
-                    }
-                }
-                else
-                {
-                    if (shiftModifier)
-                    {
-                        foreach (GameObject sf in selectedFrames)
-                        {
-                            if (HelperFunctions.containsTag("Moveable", sf.gameObject.tag))
-                                sf.GetComponent<Moveable>().queueMove(hit);
-                        }
-                    }
-                    else
-                    {
-                        foreach (GameObject sf in selectedFrames)
-                        {
-                            if (HelperFunctions.containsTag("Moveable", sf.gameObject.tag))
-                                sf.GetComponent<Moveable>().move(hit);
-                        }
-                    }
-                }
-            }
         }
     }
 
