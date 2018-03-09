9f135c57e00a589aa1a3b5a71338e3c48485c205
Alexkumar Patel <acerbus.ace@gmail.com>
2018-03-09 02:58:33 -0500

    changed movement component

--- Arboles Peligrosas/Assets/Scripts/CameraController.cs
+++ Arboles Peligrosas/Assets/Scripts/CameraController.cs
@@ -44,54 +44,24 @@ public class CameraController : MonoBehaviour
         float scrollPrecent = (transform.position.y - minDist) / (maxDist - minDist);
         float speed = Mathf.Lerp(minSpeed, maxSpeed, scrollPrecent);
 
-        transform.position += new Vector3(
-            Input.GetAxis("Horizontal") * speed * Time.deltaTime,
-            -(Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity * speed * Time.deltaTime),
-            Input.GetAxis("Vertical") * speed * Time.deltaTime
+        Vector3 velocity = new Vector3(
+            Input.GetAxis("Horizontal"),
+            0f,
+            Input.GetAxis("Vertical")
         );
 
+        //transform.position += velocity.normalized * speed * Time.deltaTime;
+        transform.position += velocity * speed * Time.deltaTime;
+
+        transform.position += new Vector3(
+            0f,
+            -(Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity * speed * Time.deltaTime),
+            0f
+        );
 
         // make sure camera's y position is not out of min/max distance
         if (transform.position.y > maxDist) transform.position = new Vector3(transform.position.x, maxDist, transform.position.z);
         if (transform.position.y < minDist) transform.position = new Vector3(transform.position.x, minDist, transform.position.z);
 
-        /*float x = Input.GetAxis("Horizontal") * acceleration * Time.deltaTime;
-        float y = -(Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity * acceleration * Time.deltaTime);
-        float z = Input.GetAxis("Vertical") * acceleration * Time.deltaTime;
-
-        if (velocity.x == 0) print("val is zero!!!");
-        else
-            print(velocity);
-
-        // sets velocity based on input axis
-        velocity += new Vector3(
-            velocity.x == 0 && x != 0 ? (x > 0 ? baseAcceleration : -baseAcceleration) : x,
-            -(Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity * acceleration * Time.deltaTime),
-            velocity.z == 0 && z != 0 ? (z > 0 ? baseAcceleration : -baseAcceleration) : z
-        );
-
-
-
-        // make sure velocity doesn't exceed max value
-        if (velocity.x > maxVelocity) velocity.x = maxVelocity;
-        if (velocity.x < -maxVelocity) velocity.x = -maxVelocity;
-        if (velocity.z > maxVelocity) velocity.z = maxVelocity;
-        if (velocity.z < -maxVelocity) velocity.z = -maxVelocity;
-
-        // decelerate, if input axis are not pressed
-        if (Input.GetAxis("Horizontal") == 0) {
-            velocity.x /= 1f + (deceleration * Time.deltaTime);
-            if (Mathf.Abs(velocity.x) < 1f) velocity.x = 0f;
-        }
-        if (Input.GetAxis("Vertical") == 0) {
-            velocity.z /= 1f + (deceleration * Time.deltaTime);
-            if (Mathf.Abs(velocity.z) < 1f) velocity.z = 0f;
-        }
-        if (Input.GetAxis("Mouse ScrollWheel") == 0) {
-            velocity.y /= 1f + (deceleration * Time.deltaTime);
-            if (Mathf.Abs(velocity.y) < 1f) velocity.y = 0f;
-        }
-
-        transform.position += velocity * Time.deltaTime;*/
     }
 }
