ce9fe9c70c0e394b8618a6e96efc18547207cf4d
Alexandre Skipper <alexandreskipper@WINDOWS2.SCS.CARLETON.CA>
2018-03-17 17:11:04 -0400

    added turret functionality

---
+++ Arboles Peligrosas/Assets/BulletController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    private float speed;
+
+    private Rigidbody rb;
+    private Vector3 startPos;
+
+    private float distTillDestruction;
+    private float damage;
+
+	// Use this for initialization
+	void Start () {
+        speed = 50f;
+        rb = GetComponent<Rigidbody>();
+
+        startPos = transform.position;
+        distTillDestruction = 10f;
+        damage = 10f;
+        rb.velocity = transform.forward * speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(startPos, transform.position) > distTillDestruction)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (HelperFunctions.containsTag("Enemy", collision.gameObject.tag))
+        {
+            //collision.gameObject.GetComponent<Something>().takeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
--- Arboles Peligrosas/Assets/Scripts/UnitControllers/TurretController.cs
+++ Arboles Peligrosas/Assets/Scripts/UnitControllers/TurretController.cs
@@ -7,17 +7,108 @@ public class TurretController : MonoBehaviour, Selectable {
     private UI_SelectedFrame sfInfo;
 
     private string unitName;
+    private Transform head;
+    public GameObject bulletPrefab;
+    private float bulletSpawnOffsetZ;
+    private float bulletSpawnOffsetY;
 
-	// Use this for initialization
-	void Start () {
+    private float fireRate;
+    private float currFireRate;
+
+    private float detectionRadius;
+    private float rotationSpeed;
+
+    private GameObject currTarget;
+
+
+    // Use this for initialization
+    void Start () {
         sfInfo.info = new Dictionary<string, string>();
         unitName = "Turret";
-	}
+
+        head = transform.Find("Head");
+        Collider c = GetComponent<Collider>();
+        float z = c.bounds.size.z / 2;
+        bulletSpawnOffsetZ = z + 0.75f;
+        bulletSpawnOffsetY = c.bounds.size.y;
+
+        fireRate = 1f;
+        currFireRate = fireRate;
+
+        detectionRadius = 10f;
+        rotationSpeed = 2f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         updateSFInfo();
-	}
+
+        if (currTarget == null || Vector3.Distance(transform.position, currTarget.transform.position) > detectionRadius)
+            currTarget = getClosestTarget();
+
+        if (rotateTowardsTarget())
+        {
+            if (currFireRate < 0)
+            {
+                shoot();
+
+                currFireRate = fireRate;
+            }
+        }
+
+        currFireRate -= Time.deltaTime;
+    }
+
+    void shoot()
+    {
+        Vector3 pos = head.position + head.forward * bulletSpawnOffsetZ + new Vector3(0, bulletSpawnOffsetY, 0);
+        //Vector3 targetDir = currTarget.transform.position - head.position;
+        Instantiate(bulletPrefab, pos, Quaternion.identity).transform.LookAt(currTarget.transform);
+        //Instantiate(bulletPrefab, pos, head.rotation);
+    }
+
+    GameObject getClosestTarget()
+    {
+        Collider[] hitCollider = Physics.OverlapSphere(transform.position, detectionRadius);
+
+        GameObject closestTarget = null;
+        foreach (Collider hit in hitCollider)
+        {
+            if (HelperFunctions.containsTag("Enemy", hit.gameObject.tag))
+            {
+                if (closestTarget == null)
+                {
+                    closestTarget = hit.gameObject;
+                }
+                else
+                {
+                    closestTarget = Vector3.Distance(transform.position, hit.gameObject.transform.position) < Vector3.Distance(transform.position, closestTarget.transform.position) ? hit.gameObject : closestTarget;
+                }
+            }
+        }
+
+        return closestTarget;
+    }
+
+    bool rotateTowardsTarget()
+    {
+        bool returnValue = false;
+
+        if (currTarget != null)
+        {
+            Vector3 targetDir = currTarget.transform.position - head.position;
+            float step = rotationSpeed * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(head.forward, targetDir, step, 0f);
+
+            print("turning distance: " + Vector3.Distance(head.forward, targetDir.normalized));
+            if (Vector3.Distance(head.forward, targetDir.normalized) < step * 2)
+                returnValue = true;
+
+            head.rotation = Quaternion.LookRotation(newDir);
+        }
+
+        return returnValue;
+    }
 
     public void setDestination(Ray ray) { }
 
